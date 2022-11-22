bab5e805123ef7681298e3b6f4c01bf590835e70
Mayada <mayadayousuf96@gmail.com>
2022-11-22 05:31:22 -0800

    printf Function

--- _printf.c
+++ _printf.c
@@ -1,55 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 
 /**
- * vprintk - handling printf cases
- * @fm: the case
- * @org: the char after the case
- *
- * Return: nothing
- */
-
-void vprintk(const char *fm, va_list org)
-{
-	int state = 0;
-	char ch;
-	const char *s;
-
-	while (*fm)
-	{
-		if (state == 0)
-		{
-			if (*fm == '%')
-				state = 1;
-			else
-				putchar(*fm);
-		}
-		else if (state == 1)
-		{
-			switch (*fm)
-			{
-				case 'c':
-					ch = va_arg(org, int);
-					putchar(ch);
-					break;
-				case 's':
-					s = va_arg(org, const char *);
-					while (*s)
-					{
-						putchar(*s++);
-					}
-					break;
-			}
-				state = 0;
-		}
-		fm++;
-	}
-}
-
-
-/**
- * _printf - produces output according to a format.
+ * _printf - print the chareckter
  * @format: the format to be printed
  *
  * Return: the espicifide string or number
@@ -58,9 +13,43 @@ void vprintk(const char *fm, va_list org)
 int _printf(const char *format, ...)
 {
 	va_list op;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
 	va_start(op, format);
-	vprintk(format, op);
-	va_end(op);
-	return (0);
+	buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{
+				print_buf(buffer, ibuf), free(buffer), va_end(op);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+					len += function(op, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+			} i++;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
+	}
+	print_buf(buffer, ibuf), free(buffer), va_end(op);
+	return (len);
 }
